0536bb34d551960ec20935799e56833f4ce977ca
Alon Albert <aalbert@google.com>
2024-11-18 12:13:41 -0800

    Add Room to Build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.androidx.room)
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
 }
 
 android {
@@ -51,6 +57,8 @@ dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.work.runtime.ktx)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
